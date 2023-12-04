51ac8b839f4c82da8bad7d74cce33a1e84acfca3
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-12-04 14:01:41 +0530

    chore - unused import cleanups

--- graph_search/graph_search.py
+++ graph_search/graph_search.py
@@ -1,12 +1,6 @@
-from gremlin_python.process.traversal import TextP, P
-# from gremlin_python.process.graph_traversal import __
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection as _DriverRemoteConnection
 from gremlin_python.process.anonymous_traversal import traversal
-from gremlin_python.structure.graph import Graph
-from gremlin_python.process.anonymous_traversal import traversal
-from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
-# from graph_search.elements import create_graph_objects
-from .type_defs import GraphTraversalConfigType, NodeFiltersConfigType
+from .type_defs import GraphTraversalConfigType
 from .traversals import  InvanaTraversalSource, InvanaTraversal, __
 from .graphson_reader import INVANA_DESERIALIZER_MAP
 from gremlin_python.structure.io.graphsonV3d0 import GraphSONReader
@@ -22,24 +16,19 @@ class DriverRemoteConnection(_DriverRemoteConnection):
 
 class GraphSearch:
 
-    # https://github.com/apache/tinkerpop/blob/095718617e0f94f92545e0ac077ca0ab189d6b7f/docs/src/reference/gremlin-variants.asciidoc#domain-specific-languages-4
-    #https://github.com/apache/tinkerpop/blob/095718617e0f94f92545e0ac077ca0ab189d6b7f/gremlin-python/src/main/python/gremlin_python/process/graph_traversal.py#L994
     def __init__(self, connection_url, ) -> None:
         self.connection_url = connection_url
         self.deserializer_map = INVANA_DESERIALIZER_MAP
-
         self.connection = DriverRemoteConnection(
             self.connection_url,  'g',
             graphson_reader=GraphSONReader(deserializer_map=self.deserializer_map),
         )
-
-        self.g: InvanaTraversal = traversal(traversal_source_class=InvanaTraversalSource) \
-                        .withRemote(self.connection)
         print(f"Graphsearch is using gremlinpython=={__version__.version}")
  
     def search_graph(self, graph_traversal_config: GraphTraversalConfigType):
-        result = self.g.search_graph(**graph_traversal_config).elementMap().toList()
+        g: InvanaTraversal = traversal(traversal_source_class=InvanaTraversalSource) \
+                        .withRemote(self.connection)
+        result = g.search_graph(**graph_traversal_config).elementMap().toList()
         self.connection.close()
         return result
-        # return create_graph_objects(_)
    
